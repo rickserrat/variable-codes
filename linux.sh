e78ba108097e8adc628b154aac39299465c6de05
Henrique S Guimaraes <henriqueserrat@gmail.com>
2017-11-21 17:44:01 -0200

    second commit

--- linux.sh
+++ linux.sh
@@ -1,10 +1,24 @@
-#!/bin/sh                                                                                 $
-HTTP_STATUS=`curl -s -o /dev/null -w "%{http_code}" http://example.org`                 $
-if [ "$HTTP_STATUS" != "200" ]                                                            $
-then                                                                                      $
-        service nginx stop                                                                $
+#!/bin/sh                                                                                 
+HTTP_STATUS=`curl -s -o /dev/null -w "%{http_code}" http://example.org`                 
+if [ "$HTTP_STATUS" != "200" ]                                                            
+then                                                                                      
+        service nginx stop                                                                
         echo "Service stop - $(date)" >> log-script.txt
         sleep 10
         service nginx restart
         echo "Service start - $(date)" >> log-script.txt
 fi
+
+....
+
+#!/bin/sh                                                                                
+HTTP_STATUS=`curl -s -o /dev/null -w "%{http_code}" http://example.org`                 
+if [ "$HTTP_STATUS" = "200" ]                                                            
+      echo "Service UP - $(date)" >> log-script.txt
+else
+        service nginx stop                                                                
+        echo "Service stop - $(date)" >> log-script.txt
+        sleep 10
+        service nginx restart
+        echo "Service start - $(date)" >> log-script.txt
+fi 
